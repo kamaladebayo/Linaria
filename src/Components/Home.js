596d5365aad0d457d25ab0fb1ad9b08823dec7cf
kamaladebayo <60827461+kamaladebayo@users.noreply.github.com>
2022-06-15 22:21:49 +0100

    SignInWithpopup enabled

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,34 +1,33 @@
 import './css/Home.css'
 import SPACESURFER from './img/spacesurfer.svg'
-import { GoogleAuthProvider, getAuth, signInWithRedirect } from "firebase/auth"
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth"
 const Home = () => {
-
+    
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
     const handleAuth = () => {
         // Firebase Auth
-        const provider = new GoogleAuthProvider();
-        const auth = getAuth();
-        signInWithRedirect(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            console.log(token);
-            console.log(user);
-            // ...
-          }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            console.log(errorCode, errorMessage, email, credential);
-            // ...
-        });
-
+        signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    console.log(token);
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log(errorCode, errorMessage, email, credential);
+    // ...
+  });
         
     }
     return ( 
